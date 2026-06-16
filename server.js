e402d9b0cc75cb09717c501221e14c7ee6a745a0
adamrobot16 <adamrobot16@gmail.com>
2026-06-16 18:22:52 +0200

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/scrape', async (req, res) => {
 
     try {
         // Cél weboldal letöltése
-        const response = await fetch('https://terkep-kereso-backend.onrender.com/scrape?url=${encodeURIComponent(url)}');
+        const response = await fetch(targetUrl);
         const html = await response.text();
 
         // HTML betöltése és elemzése a Cheerio segítségével
